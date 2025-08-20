6572a591aace2f691a75e91c05992764d578b1aa
Stracci <paulo.stracci@accenture.com>
2025-08-20 11:55:12 -0300

    package .json 6

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 // server.js
-require('dotenv').config();
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 const express = require('express');
 const session = require('express-session');
 const passport = require('passport');
