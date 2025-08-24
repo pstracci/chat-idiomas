5d7a67ee5e3ee42b5f95a937844b61fd8aaf85b2
Stracci <paulo.stracci@accenture.com>
2025-08-23 23:30:29 -0300

    fix: Altera configuração SMTP para porta 2525

--- routes/auth.js
+++ routes/auth.js
@@ -11,7 +11,7 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
     port: process.env.EMAIL_PORT,
-    secure: true,
+    secure: false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
